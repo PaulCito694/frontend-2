1ada1906209e529fb28f1b6e76392ac9e51bb7e3
PaulCito694 <paulr1604@gmail.com>
2024-10-01 20:22:48 -0500

    skip cors

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,8 +4,11 @@ const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   },
-  withCredentials: true,
+  withCredentials: false,
   withXSRFToken: true,
 })
 
