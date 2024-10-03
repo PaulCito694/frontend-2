5a27e6264f03ab4b5d86b5eb15b6ef7726f2346a
PaulCito694 <paulr1604@gmail.com>
2024-10-02 23:08:51 -0500

    array fields y avance de vista de ventas

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,15 +1,38 @@
 import React from 'react'
-import {TextField} from "@mui/material";
-import {useField} from "react-final-form";
-import clsx from "clsx";
+import { TextField } from '@mui/material'
+import { useField } from 'react-final-form'
+import clsx from 'clsx'
 
-const TextFieldField = ({name, label, className, validate, ...props}) =>{
-  const {input, meta: {error, touched}} = useField(name,{validate})
-  return <div className='grid'>
-    <label htmlFor={`${name}-input`} id={`${name}-label`} className="block font-bold mb-1 min-w-25">{label}</label>
-    <TextField className={clsx('border border-solid border-black', className) } {...input} {...props}/>
-    {touched && <span className='text-red-500'>{error}</span>}
-  </div>
+const TextFieldField = ({
+  name,
+  label,
+  className,
+  parentClassName,
+  validate,
+  ...props
+}) => {
+  const {
+    input,
+    meta: { error, touched },
+  } = useField(name, { validate })
+  return (
+    <div className={clsx('grid', parentClassName)}>
+      {label && (
+        <label
+          htmlFor={`${name}-input`}
+          id={`${name}-label`}
+          className="block font-bold mb-1 min-w-25">
+          {label}
+        </label>
+      )}
+      <TextField
+        className={clsx('border border-solid border-black', className)}
+        {...input}
+        {...props}
+      />
+      {touched && <span className="text-red-500">{error}</span>}
+    </div>
+  )
 }
 
 export default TextFieldField
